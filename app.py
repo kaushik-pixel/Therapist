8b54733b2dd06b43d366a73af88b4c84d2a44d42
kaushik-pixel <70168894+kaushik-pixel@users.noreply.github.com>
2025-03-02 05:27:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ def serve_frontend():
 def test():
     return "Backend is working!"
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
+
 # Configure Gemini API
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
